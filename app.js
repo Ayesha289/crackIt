db8702a41f284501df171b49e1d88d4b2ac27497
Ayesha <ayeshamulani928@gmail.com>
2023-03-11 12:29:37 +0530

    Connected to mongoDB and added authentiacation for login

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/quizDB');
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = mongoose.model('User', userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -27,6 +38,40 @@ app.get("/secret/register", function(req, res){
 
 app.get("/secret/login", function(req, res){
     res.render("login");
+});
+
+app.get("/secret/crackIt", function(req, res){
+    res.render("crackIt");
+});
+
+app.post("/register", function(req, res){
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save();
+        res.redirect("/secret/crackIt");
+    });
+});
+
+app.post("/login", async function(req, res){
+    const username = req.body.email;
+    const userPass = req.body.password;
+    const userFound = await User.find();
+    userFound.forEach(element => {
+        if(element.email === username){
+            bcrypt.compare(userPass, element.password, function(err, result) {
+                if(result === true){
+                    res.redirect("/secret/crackIt");
+                }
+            });
+        }
+    });
+
+    
 });
 
 app.listen(3000, function() {
